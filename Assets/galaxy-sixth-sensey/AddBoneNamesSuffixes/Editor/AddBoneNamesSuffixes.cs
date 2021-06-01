8abade624c53a5e851996a4906e23556580f2b0c
aiya000 <aiya000.develop@gmail.com>
2021-06-01 17:28:44 +0900

    :up: Import AddBoneNamesSuffixes

---
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b0c8d38c4583b4fb0ac1621ce5820c61d536d7fcf19a4a3ec9a463c96ad8318
+size 8630
---
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/BoneNamesTemplates.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:99e64dacfd5836440bedcd4100d9c99ca1a4722f34273dfacf1e4837dc8a0984
+size 3046
