048cb0a2319d17ec904c469696fb9e2a09c840e9
aiya000 <aiya000.develop@gmail.com>
2021-06-01 18:15:56 +0900

    :+1: Support generating json

--- Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34d4ea5f3e34f867dae215b1890bfdc3cca2fdd85809e7e32598bd4cabfad607
-size 8317
+oid sha256:01995a57781a09d80e89bb5a822957f6a053ccca61198403a47a4ace08df83aa
+size 8972
---
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/JSONWorkaround.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8dcd82a3e9fcd17d015d5143e535ba38f204d989f83fbc8982c23648cf10f5e
+size 181
