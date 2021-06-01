624d62028af0c6137abf7022a5de541aace9e7ac
aiya000 <aiya000.develop@gmail.com>
2021-06-01 17:47:45 +0900

    :bug: Fix an issue that Target Bone Names is not rendered for updating

--- Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b0c8d38c4583b4fb0ac1621ce5820c61d536d7fcf19a4a3ec9a463c96ad8318
-size 8630
+oid sha256:c3d93c34248566e044dacd70351cae7435807053530b1516b72f65a0fa75e22f
+size 8130
--- Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/BoneNamesTemplates.cs
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/BoneNamesTemplates.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99e64dacfd5836440bedcd4100d9c99ca1a4722f34273dfacf1e4837dc8a0984
-size 3046
+oid sha256:b37e89fbde85abb766100d6aa148c186d140078f144a28644bd4b42a41668642
+size 3067
