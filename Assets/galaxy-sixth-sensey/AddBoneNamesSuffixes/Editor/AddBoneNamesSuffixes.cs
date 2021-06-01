51371e440def027ab61889ff50c950d11a64b9c6
aiya000 <aiya000.develop@gmail.com>
2021-06-01 17:48:47 +0900

    :+1: Add a notice

--- Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3d93c34248566e044dacd70351cae7435807053530b1516b72f65a0fa75e22f
-size 8130
+oid sha256:23b88c544c9c29ec787b95d337a9ee681d8ce4d353d2f9661d9c3af34d67c72d
+size 8228
