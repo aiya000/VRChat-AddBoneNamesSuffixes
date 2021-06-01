f5382de1b306e2519b7104e5848f64bcee617a46
aiya000 <aiya000.develop@gmail.com>
2021-06-01 18:28:30 +0900

    :+1: Support loading preferences (json)

--- Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01995a57781a09d80e89bb5a822957f6a053ccca61198403a47a4ace08df83aa
-size 8972
+oid sha256:1cc3a7c0bf1bee10a1b6d2b24d0050b708c71215ffaa270d46b4262c6d81b87a
+size 10154
--- Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/JSONWorkaround.cs
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/JSONWorkaround.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8dcd82a3e9fcd17d015d5143e535ba38f204d989f83fbc8982c23648cf10f5e
-size 181
+oid sha256:d81c73089fcf1af4119c51c2cdb7fd77b8e1cd93dcf73ba4bd74c8b0cd9dd72d
+size 231
