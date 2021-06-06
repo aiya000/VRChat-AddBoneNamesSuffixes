4b391bf51529c1607e9e0e6134925d4b67369ee2
aiya000 <aiya000.develop@gmail.com>
2021-06-06 21:49:52 +0900

    :recycle: Refactor

--- Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cc3a7c0bf1bee10a1b6d2b24d0050b708c71215ffaa270d46b4262c6d81b87a
-size 10154
+oid sha256:02e9395c7a01b4098ae627bcdd24f18a7ab81567a9e12c80828399d95efc34d4
+size 10526
