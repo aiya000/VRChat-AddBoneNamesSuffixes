72f51b456135ba30d0578aef44e85eea583085d4
aiya000 <aiya000.develop@gmail.com>
2021-10-16 14:55:13 +0900

    :sparkles: Support scrolling

--- Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
+++ Assets/galaxy-sixth-sensey/AddBoneNamesSuffixes/Editor/AddBoneNamesSuffixes.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d907a59cbc6715ebb259fa37e31545a0ac0bfc43597a2d50fc32504791bcf548
-size 10856
+oid sha256:a991b059ee8f4f4570f4c03e0ef03abe74f5af1694c099a5e94d38b3b9d82a1c
+size 11028
